121fbc06b896f77b2bfc0337c670eb39a7b884fc
Viktoriia Rybalka <69701982+Sonorhc314@users.noreply.github.com>
2023-04-18 01:45:58 +0100

    change_quest

--- code/quest.py
+++ code/quest.py
@@ -7,25 +7,27 @@ import math
 class Journal:
     def __init__(self, scroll_index, current_quest):
         self.display_surface = pygame.display.get_surface()
-        self.quest1 = '''Quest: Wind potion for Emily & Swan. 
-        ~Dear Tiki, We've heard about your potionmaking talent, please create 1 wind potion for us.'''
+        self.quest1 = '''Quest: Wind potion for Emily. 
+        ~Dear Tiki, I've heard about your potionmaking talent, please create 1 wind potion for me.'''
         self.quest2 = '''Quest: Shining potion for Wendy. 
         ~Dear Tiki, The nights are getting dark and scary, I just can't sleep, please create 1 shining potion for me.'''
         self.quest3 = '''Quest: Fire potion for King Arthur. 
         ~Dear mighty forest witch, please create 1 fire potion for me so that I can destroy my enemies.'''
         self.quests = {1:self.quest1, 2: self.quest2, 3: self.quest3}
         self.current_quest = current_quest
-        self.dialog = dialog.Dialog(self.quests[self.current_quest], f"Quest {self.current_quest}")
+        self.dialog = dialog.Dialog(self.quests[self.current_quest], f"Quest {self.current_quest}", delimiter=' ')
         self.scroll_index=scroll_index
         self.max_length= self.dialog.get_rows_in_text_box()
     def update_journal(self, new_scroll_index):
-        self.dialog.set_text(self.quests[self.current_quest])
+        self.current_text = f"Quest {self.current_quest}"
+        self.dialog.set_text(self.quests[self.current_quest], self.current_text)
         self.dialog.set_scroll_index(new_scroll_index)
         self.scroll_index=new_scroll_index
         self.max_length= math.ceil(self.dialog.get_rows_in_text_box()/3)
     def change_quest(self, new_quest):
         self.current_quest = new_quest
-        self.dialog.set_text(self.quests[self.current_quest])
+        self.current_text = f"Quest {self.current_quest}"
+        self.dialog.set_text(self.quests[self.current_quest], self.current_text)
     def display(self):
         self.dialog.display()
     
